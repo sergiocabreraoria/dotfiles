ec6b31bcdb30c6c2fe957ab93e14d13fc3e6bc6e
Sergio Cabrera Oria <sergio.cabrera@affirm.com>
2023-03-10 09:18:58 +0100

    init

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sym() {
+    dotdir="$1"
+
+    for filename in $(find $dotdir -name "*.symlink"); do
+      ln -sf $filename ~/.$(basename $filename | sed "s/\.symlink//")
+    done
+}
+
+dotfs=$(cd $(dirname $0) && pwd)
+
+sym "$dotfs"
+
+git clone git@github.com:sergiocabreraoria/SecretDotfiles.git ~/.dotfiles/SecretDotfiles
+
+export PATH=$PATH:~/.dotfiles/SecretDotfiles
+
+~/.dotfiles/SecretDotfiles/install.sh
+
